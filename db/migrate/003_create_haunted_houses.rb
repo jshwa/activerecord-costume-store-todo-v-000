af321733c7a3ba7a5d7ea99560402cfca7e3f2b1
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-29 22:39:13 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :hauntedhouse do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end
+end
